084bca7c05bc20cd0fbf8d847ff8bd0739a1af21
oqy <qingyong.ou@gmail.com>
2013-01-16 17:23:25 +0800

    part4

---
+++ src/main/java/com/tex100/webapp/core/model/assistant/ISeqCodeGenStrategy.java
@@ -0,0 +1,14 @@
+package com.tex100.webapp.core.model.assistant;
+
+/**
+ * @author qingyong_ou
+ * 顺序编码生成策略
+ */
+public interface ISeqCodeGenStrategy {
+	/**
+	 * 生成顺序号
+	 * @param seqCodeObject
+	 * @return
+	 */
+	public String genSeqCode(ISeqCodeObject seqCodeObject);
+}
