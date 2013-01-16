7e3387d2fd570287795a4a79e4e87ee08b7ec8dd
oqy <qingyong.ou@gmail.com>
2013-01-16 17:19:52 +0800

    part1

---
+++ src/main/java/com/tex100/webapp/core/exception/BaseException.java
@@ -0,0 +1,31 @@
+package com.tex100.webapp.core.exception;
+
+import java.text.MessageFormat;
+
+public abstract class BaseException extends RuntimeException {
+	private static final long serialVersionUID = 4961603576232183799L;
+
+	public BaseException() {
+		super();
+	}
+
+	public BaseException(String message) {
+		super(message);
+	}
+
+	public BaseException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public BaseException(Throwable cause) {
+		super(cause);
+	}
+	
+	public  BaseException(ExceptionMessage exceptionMessage){
+		super(exceptionMessage.getExceptionMessage());
+	}
+	
+	public  BaseException(ExceptionMessage exceptionMessage,Object[] params){
+		super(MessageFormat.format(exceptionMessage.getExceptionMessage(),params));
+	}
+}
---
+++ src/main/java/com/tex100/webapp/core/exception/EntityException.java
@@ -0,0 +1,34 @@
+package com.tex100.webapp.core.exception;
+
+
+public class EntityException extends BaseException {
+	private static final long serialVersionUID = 7322667667588541195L;
+	
+	public static final ExceptionMessage ENTITY_ID_NOT_EXIST = new ExceptionMessage("Entity_ID_Not_Exist");
+	public static final ExceptionMessage ENTITY_OBJECT_ID_GENERATE_ERROR = new ExceptionMessage("Entity_Object_ID_Generate_Error");
+	public static final ExceptionMessage ENTITY_DATA_ACCESS_ERROR = new ExceptionMessage("Entity_Data_Access_Error");
+
+	public EntityException() {
+		super();
+	}
+
+	public EntityException(String message) {
+		super(message);
+	}
+
+	public EntityException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public EntityException(Throwable cause) {
+		super(cause);
+	}
+	
+	public  EntityException(ExceptionMessage exceptionMessage){
+		super(exceptionMessage);
+	}
+	
+	public  EntityException(ExceptionMessage exceptionMessage,Object[] params){
+		super(exceptionMessage,params);
+	}
+}
